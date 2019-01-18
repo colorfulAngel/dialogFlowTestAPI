f3d2985b1f5f18a830383c69fe62b797b234cb1b
Angel.tsai <angel.tsai@deltaww.com>
2019-01-18 16:39:25 +0800

    send different response based on intents

--- index.js
+++ index.js
@@ -5,17 +5,50 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const API_KEY = require('./apiKey');
 
-const server = express();
-server.use(bodyParser.urlencoded({
-    extended: true
-}));
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-server.use(bodyParser.json());
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
 
-server.post('/get-movie-details', (req, res) => {
+// middleware to use for all requests
+router.use(function(req, res, next){
+	// do logging
+	console.log('Something is happening.');
+	next();		// make sure we go to the next routes and don't stop here
+});
 
-    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+
+router.post('/', (req, res) => {
+	var intent = req.body.queryResult && req.body.queryResult.intent && req.body.queryResult.intent.displayName ? req.body.queryResult.intent.displayName : 'movie-intent';
+	console.log('intent:',intent);
+	
+	if(intent == 'Response Helpful Intent - yes'){
+		res.send('Thank you for your help and hope you enjoy using it.');
+	}
+	else if(intent == 'Response Helpful Intent - no'){
+		res.send('We\'ll working hard to improve it!');
+	}
+	else {
+		getMovie(req, res);
+	}
+	
+    
+});
+
+
+function getResponseScore(req, res){
+	var score = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.score ? req.body.queryResult.parameters.score : 4;
+	
+	res.json({message:"Thank you for giving us score ", score})
+}
+
+function getMovie(req, res){
+	var movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+	
+	const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
@@ -67,8 +100,27 @@ server.post('/get-movie-details', (req, res) => {
             source: 'get-movie-details'
         });
     });
-});
+}
 
-server.listen((process.env.PORT || 8000), () => {
-    console.log("Server is up and running...");
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/res-helpful')
+	// create a bear (accessed at POST http://localhost:8080/get-movie-details/res-helpful)
+    .post(function(req, res){
+		console.log(req.body);
+		var name = req.body.name || 'Admin';
+		var score = req.body.score || 5;
+		res.send('Hello '+name+'. Thank you for giving us score:'+score);
+	});
+	
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/get-movie-details', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen((process.env.PORT || 8000), () => {
+    console.log("server is up and running...");
 });
